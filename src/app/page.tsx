f486ee65fd6776dc498b614c059a64b66646f16b
DF2402 <your.email@example.com>
2026-05-27 23:41:04 +0800

    feat: framework of weather dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,8 +43,71 @@ export default function App() :React.JSX.Element {
     }
   }, [coordinates]);
   return(
-    <div className="flex min-h-screen justify-center bg-bg gap-8 p-8"> 
-      <div className="flex flex-col items-center justify-center w-[340px] h-[480] rounded-3xl bg-bg shadow-convex ">
+    <div className="flex w-full min-h-screen justify-center bg-bg "> 
+
+      <div className=' grid grid-rows-8 w-full max-w-[2400px] gap-8 p-10 '>  
+
+        <div className="row-span-1 w-full h-[152px] rounded-3xl bg-bg shadow-convex">
+
+          <div className='flex flex-col justify-between p-2' >
+
+            <div className="flex items-center justify-center w-[100px] h-[40px] rounded-full shadow-concave gap-4">
+
+              <div className="text-gauge"> 天氣警報</div>
+
+            </div>
+
+            <div id="warning=" className="flex items-center justify-start gap-4 p-4">
+              <img src="https://www.hko.gov.hk/images/HKOWarningSymbols/warn800_20_hot.png" alt="warning" className="w-[70px] h-[70px]" />
+            </div>
+
+          </div>
+
+
+        </div>
+
+        <div id="" className="grid grid-cols-3 row-span-4 w-full  gap-8  "> 
+
+          <div id="left column " className="col-span-2 grid grid-row-2 gap-8">
+
+            <div className=" items-center justify-center w-full h-[480px] rounded-3xl bg-bg shadow-convex ">
+                
+            </div>
+
+            <div className=" items-center justify-center w-full h-[200px] rounded-3xl bg-bg shadow-convex ">
+                  
+              </div>
+
+            
+            
+          </div>
+
+          <div id="right column" className="grid grid-row-2 w-full gap-8 ">
+
+              <div className=" items-center justify-center w-full h-[280px] rounded-3xl bg-bg shadow-convex ">
+
+            </div>
+
+              <div className="items-center justify-center w-full h-[400px] rounded-3xl bg-bg shadow-convex ">
+                  
+              </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+function weatherWidget() : React.JSX.Element {
+  const loading = true;
+  const coordinates = { latitude: 0, longitude: 0 };
+  const temp = 25;
+  const city = "Unknown";
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center w-[340px] h-[480] rounded-3xl bg-bg shadow-convex ">
           <div className="relative flex items-center justify-center h-[240px] w-[240px]  rounded-full bg-bg shadow-convex">
             <div className="relative flex items-center justify-center h-[220px] w-[220px] rounded-full bg-bg shadow-concave">
               <div className="relative flex items-center justify-center h-[170px] w-[170px] rounded-full bg-bg shadow-convex">
@@ -71,7 +134,6 @@ export default function App() :React.JSX.Element {
             </div>
           </div>
         </div>
-        
         <div className="flex flex-col items-center justify-center w-[180px] h-[100] rounded-3xl bg-bg shadow-convex">
               <div className="text-2xl font-bold text-center text-gauge"> location  </div>
               {
@@ -88,8 +150,5 @@ export default function App() :React.JSX.Element {
                 )
               }
         </div>
-
-    
-      </div>
-  )
-}
+      </>
+  )}
